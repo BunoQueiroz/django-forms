ad0c718d28ed3dec87225e24cc83562398a18c3d
Bruno Castro <bs8872491@gmail.com>
2022-11-22 15:36:05 -0300

    Adicionando novos campos ao formulário e criando os CHOICES

---
+++ passagens/choices.py
@@ -0,0 +1,6 @@
+tipos_classe = {
+    ('1', 'econômica'),
+    ('2', 'execultiva'),
+    ('3', 'primeira'),
+    ('4', 'master')
+}
--- passagens/forms.py
+++ passagens/forms.py
@@ -1,8 +1,19 @@
 from django import forms
 from tempus_dominus.widgets import DatePicker
+from datetime import datetime
+from .choices import tipos_classe
 
 class PassagensForms(forms.Form):
     origem = forms.CharField(label='Origem', max_length=50, strip=True, required=True)
     destino = forms.CharField(label='Destino', max_length=50, strip=True, required=True)
-    data_ida = forms.DateField(label='Data de ida', required=True, widget=DatePicker())
-    data_volta = forms.DateField(label='Data de volta', required=True, widget=DatePicker())
+    data_ida = forms.DateField(label='Data de ida', required=True, widget=DatePicker(), initial=datetime.today)
+    data_volta = forms.DateField(label='Data de volta', required=True, widget=DatePicker(), initial=datetime.today)
+    data_pesquisa = forms.DateField(label='Data da pesquisa', required=True, disabled=True, initial=datetime.today)
+    detalhes = forms.CharField(
+        label='Informações extras',
+        widget=forms.Textarea(),
+        required=False,
+        max_length=200
+    )
+    classe = forms.ChoiceField(choices=tipos_classe, initial='1')
+    email = forms.EmailField(max_length=100, required=True)

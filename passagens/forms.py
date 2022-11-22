302423725deac8904b29014296f56aeb42d0a4e5
Bruno Castro <bs8872491@gmail.com>
2022-11-22 14:03:07 -0300

    'Obrigando' o usuário a enviar datas atraves dos respectivos inputs

--- passagens/forms.py
+++ passagens/forms.py
@@ -1,7 +1,8 @@
 from django import forms
+from tempus_dominus.widgets import DatePicker
 
 class PassagensForms(forms.Form):
     origem = forms.CharField(label='Origem', max_length=50, strip=True, required=True)
     destino = forms.CharField(label='Destino', max_length=50, strip=True, required=True)
-    data_ida = forms.DateField(label='Data de ida', required=True)
-    data_volta = forms.DateField(label='Data de volta', required=True)
+    data_ida = forms.DateField(label='Data de ida', required=True, widget=DatePicker())
+    data_volta = forms.DateField(label='Data de volta', required=True, widget=DatePicker())
